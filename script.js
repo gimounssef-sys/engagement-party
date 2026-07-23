b85b3a114dccb8a799b2106e60a39b22d3cbed17
gimounssef-sys <gimounssef@gmail.com>
2026-07-23 10:15:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const e=document.querySelector('.envelope');
+const flap=document.querySelector('.flap');
+const seal=document.querySelector('.seal');
+e.addEventListener('click',()=>{
+ seal.animate([{transform:'translate(-50%,-50%) scale(1)'},{transform:'translate(-50%,-70%) scale(.85)',opacity:0}],{duration:500,fill:'forwards'});
+ setTimeout(()=>{flap.style.transform='rotateX(180deg)'},250);
+});
